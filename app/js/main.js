55b84bfa35acdd63d5820bcbf7532fc8a5812378
Sanjay Rajashekhar <91sanjay@gmail.com>
2016-01-15 17:08:01 -0500

    Add flick API code and fix marker clickclose event handling

--- app/js/main.js
+++ app/js/main.js
@@ -62,11 +62,11 @@
         this.title = data.title;
         this.coordinates = data.coordinates;
         this.placeIdUrl = "https://api.flickr.com/services/rest/?method=flickr.places.findByLatLon" +
-            "&api_key=a12bbfe0b4dd66e0550265affea11271&lat=" + data.coordinates.lat + "&lon=" + data.coordinates.lng +
+            "&api_key=360da6373d34f0076c66dedc9bf6270d&lat=" + data.coordinates.lat + "&lon=" + data.coordinates.lng +
             "&format=json&nojsoncallback=1";
         this.flickrUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.search" +
-            "&api_key=a12bbfe0b4dd66e0550265affea11271&text=" + data.title + "&lat=" + data.coordinates.lat +
-            "&lon=" + data.coordinates.lng + "&place_id=[placeid]&per_page=10&format=json&nojsoncallback=1";
+            "&api_key=360da6373d34f0076c66dedc9bf6270d&text=" + data.title + "&lat=" + data.coordinates.lat +
+            "&lon=" + data.coordinates.lng + "[placeid]&per_page=10&format=json&nojsoncallback=1";
     };
 
     /*
@@ -117,6 +117,10 @@
         init();
 
         ko.utils.arrayForEach(this.markersArray(), function (marker) {
+            google.maps.event.addListener(marker, 'click', function() {
+                map.panTo(marker.getPosition());
+            });
+
             marker.addListener('click', toggleBounce);
             function toggleBounce() {
                 if (marker.getAnimation() !== null) {
@@ -129,32 +133,29 @@
                 }
             }
 
-            marker.addListener('click', openInfoWindow);
-            function openInfoWindow() {
-                var infoWindow = new google.maps.InfoWindow({
-                    content: marker.title,
-                    maxWidth: 600
-                });
+            var infoWindow = new google.maps.InfoWindow({
+                content: marker.title,
+                maxWidth: 600
+            });
 
+            google.maps.event.addListener(marker,'click', openInfoWindow);
+            function openInfoWindow() {
                 infoWindow.open(map, marker);
+                highlightListElement(marker);
             }
+
+            google.maps.event.addListener(infoWindow, "closeclick", function() {
+                deselectListElement(marker);
+            });
         });
 
         self.selectMarker = function (marker) {
             highlightListElement(marker);
 
-            var placeid;
             marker.setAnimation(google.maps.Animation.BOUNCE);
             setTimeout(function () {
                 marker.setAnimation(null);
             }, 2000);
-
-            //getPlaceId(marker.placeIdUrl);
-            //var flickrUrl = marker.flickrUrl;
-            //flickrUrl = flickrUrl.replace("[placeid]", placeid);
-            //console.log(flickrUrl);
-
-            //var infoContent = getFlickrData(flickrUrl);
 
             var infoWindow = new google.maps.InfoWindow({
                 content: marker.title,
@@ -162,13 +163,71 @@
             });
 
             infoWindow.open(map, marker);
+
+            google.maps.event.addListener(infoWindow, "closeclick", function() {
+                deselectListElement(marker);
+            });
+
+            getPlaceId(marker.placeIdUrl)
+                .done(function(response) {
+                    console.log("enter");
+                    var flickrUrl;
+                    if (response.stat == "fail") {
+                        console.log("API returned error while retrieving palce if");
+                        flickrUrl = marker.flickrUrl.replace("[placeid]", '');
+                    } else {
+                        flickrUrl = marker.flickrUrl.replace("[placeid]", "&place_id="+response.places.place[0].place_id);
+                        getFlickrPhotos(flickrUrl)
+                            .done(function(photosHolder) {
+                                if (photosHolder.stat == "fail") {
+                                    console.log("API returned error while retrieving photos");
+                                } else {
+                                    console.log(photosHolder.photos.photo.length);
+                                }
+                            });
+                    }
+                }).fail(function(){
+                    console.log("Failed");
+                });
         };
 
         var highlightListElement = function (marker) {
-            var listElements = document.getElementsByClassName("place");
             var listElement = document.getElementById(marker.id);
 
             listElement.className += " active";
+        };
+
+        var deselectListElement = function(marker) {
+            var listElement = document.getElementById(marker.id);
+
+            listElement.className = "place";
+        };
+
+        var getPlaceId = function (placeUrl) {
+            var deferred = $.Deferred();
+
+            $.getJSON(placeUrl).then(function(response) {
+                if(!response) {
+                    deferred.reject(response);
+                }
+                deferred.resolve(response);
+            });
+            return deferred.promise();
+        };
+
+        var getFlickrPhotos = function(flickrUrl) {
+            var deferred = $.Deferred();
+
+            $.getJSON(flickrUrl)
+                .then(function(response) {
+                    if(!response) {
+                        deferred.reject(response);
+                    }
+
+                    deferred.resolve(response);
+                });
+
+            return deferred.promise();
         };
 
         this.filter = function () {
@@ -196,33 +255,7 @@
                 }
             });
         };
-
-        var getPlaceId = function (url) {
-            $.ajax({
-                url: url,
-                method: 'GET',
-                datatype: 'JSON',
-                success: function (response) {
-                    return response.palces.place.place_id;
-                }
-            });
-        };
-
-        var getFlickrData = function (url) {
-            $.ajax({
-                url: url,
-                method: 'GET',
-                dataType: 'JSON',
-                success: function (response) {
-                    console.log(response.photos);
-                    return "Space";
-                }
-            });
-        };
-
-        getFlickrData();
     };
 
-    var viewModel = new ViewModel();
-    ko.applyBindings(viewModel);
+    ko.applyBindings(new ViewModel());
 })();
